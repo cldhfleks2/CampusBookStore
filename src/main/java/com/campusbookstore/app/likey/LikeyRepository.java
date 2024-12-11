45fa35d35fe1c5f2e617a427c29ed3100ae46461
cldhfleks2 <cldhfleks2@naver.com>
2024-12-11 16:51:19 +0900

    fix[LikeyRepository]: create findByName method
    [fixed: change return type]

--- src/main/java/com/campusbookstore/app/likey/LikeyRepository.java
+++ src/main/java/com/campusbookstore/app/likey/LikeyRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LikeyRepository extends JpaRepository<Likey, Long> {
-    Optional<Member> findByName(String name);
+    Optional<Likey> findByName(String name);
 
     @Query("SELECT l FROM Likey l WHERE l.status = 1")
     List<Likey> findAllByStatus();
