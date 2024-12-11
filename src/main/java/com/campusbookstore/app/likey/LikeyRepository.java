23069e8f6426c14a60549cc7c4751ec8b328c53f
cldhfleks2 <cldhfleks2@naver.com>
2024-12-11 16:50:16 +0900

    feat[LikeyRepository]: create findByName method

--- src/main/java/com/campusbookstore/app/likey/LikeyRepository.java
+++ src/main/java/com/campusbookstore/app/likey/LikeyRepository.java
@@ -1,11 +1,15 @@
 package com.campusbookstore.app.likey;
 
+import com.campusbookstore.app.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikeyRepository extends JpaRepository<Likey, Long> {
+    Optional<Member> findByName(String name);
+
     @Query("SELECT l FROM Likey l WHERE l.status = 1")
     List<Likey> findAllByStatus();
 
